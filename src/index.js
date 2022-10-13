70e19e0d89430fab905b654f7df619d460c46373
Ashis Chaudhary <ashischaudhary959@gmail.com>
2022-10-13 16:25:15 +1100

    added add-reaction method

--- src/index.js
+++ src/index.js
@@ -24,25 +24,43 @@ async function main(event, context) {
     }
   }
 
-  if (
-    !body.event.bot_id &&
-    body.event.client_msg_id &&
-    body.event.type === "message"
-  ) {
-    var text = `<@${body.event.user}> isn't AWS Lambda awesome?`;
+  if (!body.event.bot_profile) {
+    // var text = `<@${body.event.user}> isn't AWS Lambda awesome?`;
 
     const url = "https://slack.com/api/chat.postMessage";
     const res = await axios.post(
       url,
-      { channel: body.event.channel, text: text },
+      { channel: body.event.channel, text: "Hello World!" },
       {
         headers: {
           authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
-          "Content-Type": "application/json;charset=utf-8",
+          "Content-Type": "application/json; charset=utf-8",
         },
       }
     );
     console.log("RESULT: ", res.data);
+  }
+
+  const message = body.event.blocks[0].elements[0].elements[0].text;
+
+  if (message.includes("hi")) {
+    const url = "https://slack.com/api/reactions.add";
+    const res = await axios.post(
+      url,
+      {
+        channel: body.event.channel,
+        name: "eye",
+        timestamp: body.event.event_ts,
+      },
+      {
+        headers: {
+          authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+          "Content-Type":
+            "application/json; application/x-www-form-urlencoded; charset=utf-8",
+        },
+      }
+    );
+    console.log("Done", res.data);
   }
 }
 module.exports = { main };
